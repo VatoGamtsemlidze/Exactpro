b7b28f1ebca50001aa50ff8a2cf9ea19bab02815
Vato Gamtsemlidze <vatogamtsemlidze@gmail.com>
2022-09-11 13:10:09 +0400

    img fix

--- src/layout/Mission.js
+++ src/layout/Mission.js
@@ -1,73 +1,80 @@
-import React from 'react';
-import {Box, Grid, makeStyles, useMediaQuery} from "@material-ui/core";
-
-
+import React from "react";
+import { Box, Grid, makeStyles, useMediaQuery } from "@material-ui/core";
 
 const useStyles = makeStyles({
-    image:{
-        maxWidth:"100%"
+  image: {
+    maxWidth: "100%",
+  },
+  title: {
+    color: "#333333",
+    padding: 0,
+    margin: 0,
+  },
+  missionParagraph: {
+    animationName: "$entrance",
+    animationDuration: "2s",
+  },
+  missionImage: {
+    animationName: "$entranceLeft",
+    animationDuration: "2s",
+  },
+  "@keyframes entrance": {
+    "0%": {
+      opacity: 0,
+      transform: "translateX(-200%)",
     },
-    title:{
-        color: "#333333",
-        padding:0,
-        margin:0
+    "100%": {
+      opacity: 1,
+      transform: "translateY(0)",
     },
-    missionParagraph:{
-        animationName:"$entrance",
-        animationDuration:"2s"
+  },
+  "@keyframes entranceLeft": {
+    "0%": {
+      opacity: 0,
+      transform: "translateX(200%)",
     },
-    missionImage:{
-        animationName:"$entranceLeft",
-        animationDuration:"2s"
+    "100%": {
+      opacity: 1,
+      transform: "translateY(0)",
     },
-    "@keyframes entrance": {
-        "0%": {
-            opacity: 0,
-            transform: "translateX(-200%)"
-        },
-        "100%": {
-            opacity: 1,
-            transform: "translateY(0)"
-        }
-    },
-    "@keyframes entranceLeft": {
-        "0%": {
-            opacity: 0,
-            transform: "translateX(200%)"
-        },
-        "100%": {
-            opacity: 1,
-            transform: "translateY(0)"
-        }
-    },
-})
+  },
+});
 
 const Mission = () => {
+  const classes = useStyles();
+  const fullWidth = useMediaQuery("(min-width: 960px)"); //to generate font-size: 18px only from
+  const fontTransform = useMediaQuery("(max-width: 850px)"); // 850pxs to 960px
 
-    const classes = useStyles();
-    const fullWidth = useMediaQuery('(min-width: 960px)')   //to generate font-size: 18px only from
-    const fontTransform = useMediaQuery('(max-width: 850px)') // 850pxs to 960px
-
-    return (
-        <Box pt={5}>
-            <Grid container>
-                <Grid item xs={12} md={6} className={classes.missionParagraph}>
-                    <h1 className={classes.title}>Our Mission</h1>
-                    <hr width="95px" color="#65ACB2" align="left" size="1"/>
-                    <p style={{fontSize: !fullWidth && !fontTransform ? "18px" : "16px"}}>
-                        Exactpro specializes in quality assurance services and related software
-                        development with focus on test automation for securities data distribution,
-                        trading systems, risk management, market surveillance and post-trade infrastructures.
-                        <br/><br/>
-                        We use bespoke tools and methods for every challenge and do whatever it takes to help our clients.</p>
-                        <br/>
-                </Grid>
-                <Grid item xs={12} md={6} className={classes.missionImage}>
-                    <img className={classes.image} src="https://bit.ly/3zJZxqK" alt=""/>
-                </Grid>
-            </Grid>
-        </Box>
-    );
+  return (
+    <Box pt={5}>
+      <Grid container>
+        <Grid item xs={12} md={6} className={classes.missionParagraph}>
+          <h1 className={classes.title}>Our Mission</h1>
+          <hr width="95px" color="#65ACB2" align="left" size="1" />
+          <p
+            style={{ fontSize: !fullWidth && !fontTransform ? "18px" : "16px" }}
+          >
+            Exactpro specializes in quality assurance services and related
+            software development with focus on test automation for securities
+            data distribution, trading systems, risk management, market
+            surveillance and post-trade infrastructures.
+            <br />
+            <br />
+            We use bespoke tools and methods for every challenge and do whatever
+            it takes to help our clients.
+          </p>
+          <br />
+        </Grid>
+        <Grid item xs={12} md={6} className={classes.missionImage}>
+          <img
+            className={classes.image}
+            src="https://exactpro.com/sites/default/files/styles/news_teaser/public/media/mission.jpg?itok=0FKfqVsX"
+            alt=""
+          />
+        </Grid>
+      </Grid>
+    </Box>
+  );
 };
 
 export default Mission;
--- src/layout/Overview/Overview.js
+++ src/layout/Overview/Overview.js
@@ -1,51 +1,65 @@
-import React from 'react';
-import {Box, Container,Grid, useMediaQuery} from "@material-ui/core";
-import {useStyles} from "./OverviewStyles";
+import React from "react";
+import { Box, Container, Grid, useMediaQuery } from "@material-ui/core";
+import { useStyles } from "./OverviewStyles";
 
 const Overview = () => {
+  const classes = useStyles();
+  const noPadding = useMediaQuery("(min-width: 600px)");
 
-    const classes = useStyles();
-    const noPadding = useMediaQuery('(min-width: 600px)')
-
-    return (
-        <Box bgcolor="#F0F0F0" pt={2} marginTop={4} pb={6}>
-            <Container style={{padding: noPadding ? "0 80px" : "0 20px"}}>
-                <Box display="flex" justifyContent="center">
-                    <Box>
-                        <h1 className={classes.title}>What we do</h1>
-                        <hr width="100px" size="1px" color="#65ACB2" />
-                    </Box>
-                </Box>
-                <Grid container className={classes.mainGrid} spacing={2}>
-                    <Grid item lg={3} md={6} xs={12}>
-                        <Box>
-                            <img src="https://bit.ly/3ofWAMb" alt=""/>
-                            <h2>Case Studies</h2>
-                        </Box>
-                    </Grid>
-                    <Grid item lg={3} md={6} xs={12}>
-                        <Box>
-                            <img src="https://bit.ly/3m4EYAv" alt=""/>
-                            <h2>Solutions</h2>
-                        </Box>
-                    </Grid>
-                    <Grid item lg={3} md={6} xs={12}>
-                        <Box>
-                            <img src="https://bit.ly/3oiPIhd" alt=""/>
-                            <h2>Exactpro Overview</h2>
-                        </Box>
-                    </Grid>
-                    <Grid item lg={3} md={6} xs={12}>
-                        <Box>
-                            <img src="https://bit.ly/3oenN27" alt=""/>
-                            <h2>Clients & Partners</h2>
-                        </Box>
-                    </Grid>
-
-                </Grid>
-            </Container>
+  return (
+    <Box bgcolor="#F0F0F0" pt={2} marginTop={4} pb={6}>
+      <Container style={{ padding: noPadding ? "0 80px" : "0 20px" }}>
+        <Box display="flex" justifyContent="center">
+          <Box>
+            <h1 className={classes.title}>What we do</h1>
+            <hr width="100px" size="1px" color="#65ACB2" />
+          </Box>
         </Box>
-    );
+        <Grid container className={classes.mainGrid} spacing={2}>
+          <Grid item lg={3} md={6} xs={12}>
+            <Box>
+              <img
+                style={{ maxWidth: "8em" }}
+                src="https://exactpro.com/themes/expro_theme/assets/images/pages/main/3а.svg"
+                alt=""
+              />
+              <h2>Case Studies</h2>
+            </Box>
+          </Grid>
+          <Grid item lg={3} md={6} xs={12}>
+            <Box>
+              <img
+                style={{ maxWidth: "8em" }}
+                src="https://exactpro.com/themes/expro_theme/assets/images/pages/main/4а.svg"
+                alt=""
+              />
+              <h2>Solutions</h2>
+            </Box>
+          </Grid>
+          <Grid item lg={3} md={6} xs={12}>
+            <Box>
+              <img
+                style={{ maxWidth: "8em" }}
+                src="https://exactpro.com/themes/expro_theme/assets/images/pages/main/7a.svg"
+                alt=""
+              />
+              <h2>Exactpro Overview</h2>
+            </Box>
+          </Grid>
+          <Grid item lg={3} md={6} xs={12}>
+            <Box>
+              <img
+                style={{ maxWidth: "8em" }}
+                src="https://exactpro.com/themes/expro_theme/assets/images/pages/main/2а.svg"
+                alt=""
+              />
+              <h2>Clients & Partners</h2>
+            </Box>
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  );
 };
 
 export default Overview;
